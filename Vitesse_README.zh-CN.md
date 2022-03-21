95ad8290cbaec7b8b88205834fdf82280c933996
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-03-21 21:26:34 +0800

    init: qblog

--- Vitesse_README.zh-CN.md
+++ Vitesse_README.zh-CN.md
@@ -145,11 +145,11 @@ pnpm i # 如果你没装过 pnpm, 可以先运行: npm install -g pnpm
 
 使用此模板时，请尝试按照清单正确更新您自己的信息
 
-- [ ] 在 `LICENSE` 中改变作者名
-- [ ] 在 `App.vue` 中改变标题
+- [x] 在 `LICENSE` 中改变作者名
+- [x] 在 `App.vue` 中改变标题
 - [ ] 在 `vite.config.ts` 更改主机名
 - [ ] 在 `public` 目录下改变favicon
-- [ ] 移除 `.github` 文件夹中包含资助的信息
+- [x] 移除 `.github` 文件夹中包含资助的信息
 - [ ] 整理 README 并删除路由
 
 紧接着, 享受吧 :)
--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,14 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'Q-Blog',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'liuly 的静态博客' },
   ],
 })
 </script>
 
 <template>
+  <p>modify here?...</p>
   <router-view />
 </template>
