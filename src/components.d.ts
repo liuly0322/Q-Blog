a7251d5194f965c75cbf3088b818b02af58d650e
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-03-22 09:16:00 +0800

    fix: build with vite instead of vite-ssg

--- src/components.d.ts
+++ src/components.d.ts
@@ -8,11 +8,8 @@ declare module 'vue' {
     Footer: typeof import('./components/Footer.vue')['default']
     Header: typeof import('./components/Header.vue')['default']
     NAvatar: typeof import('naive-ui')['NAvatar']
-    NButton: typeof import('naive-ui')['NButton']
     NCard: typeof import('naive-ui')['NCard']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
-    NDropdown: typeof import('naive-ui')['NDropdown']
-    NIcon: typeof import('naive-ui')['NIcon']
     NMenu: typeof import('naive-ui')['NMenu']
     README: typeof import('./components/README.md')['default']
   }
