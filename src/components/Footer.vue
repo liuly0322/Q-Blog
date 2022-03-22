9e5d84282722a07a08f8acb450c2df27cdc2cfc2
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-03-22 14:59:05 +0800

    chore: dead code

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts">
 import { toggleDark } from '~/composables'
-
-const { t, availableLocales, locale } = useI18n()
-
-const toggleLocales = () => {
-  // change to some real logic
-  const locales = availableLocales
-  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
-}
 </script>
 
 <template>
