76f7832a7003575e559be1b59d02a123e9e57d45
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-04-02 12:24:15 +0800

    style: drak mode config

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
 import { darkTheme } from 'naive-ui'
-const { isDark } = useDarks()
-const darkOverrides = {
-  Layout: {
-    color: 'rgb(24, 24, 28)',
-  },
-}
+const { isDark, darkOverrides } = useDarks()
+
 const theme = computed(() => isDark.value ? darkTheme : undefined)
-const Overrides = computed(() => isDark.value ? darkOverrides : undefined)
+const overrides = computed(() => isDark.value ? darkOverrides : undefined)
 </script>
 
 <template>
-  <n-config-provider :theme="theme" :theme-overrides="Overrides">
+  <n-config-provider :theme="theme" :theme-overrides="overrides">
     <router-view />
   </n-config-provider>
 </template>
--- src/composables/useDarks.ts
+++ src/composables/useDarks.ts
@@ -1,4 +1,9 @@
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
+const darkOverrides = {
+    Layout: {
+      color: 'rgb(24, 24, 28)',
+    },
+  }
 
-export default () => ({ isDark, toggleDark })
+export default () => ({ isDark, toggleDark, darkOverrides })
