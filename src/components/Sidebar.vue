6e0a0f8f1c32e4e39d0e457d6a90cfa9f67dcbbc
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-04-07 15:18:45 +0800

    style: css

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,34 +1,48 @@
+<script setup lang="ts">
+const { isDark, toggleDark } = useDarks()
+
+const { isTablet } = usePhone()
+const router = useRouter()
+const phoneNav = (path: string) => {
+  (document.querySelector('.nav-sider') as HTMLElement).click()
+  router.push(path)
+}
+
+const { data } = useFetch('https://v1.hitokoto.cn').json()
+const hitokoto = computed(() => ({ from: data.value?.from, hitokoto: data.value?.hitokoto, from_who: data.value?.from_who }))
+</script>
+
 <template>
   <div>
-    <n-card v-if="isTablet" title="导航" content-style="display: flex; flex-wrap:wrap">
-      <span class="nav-item" @click="phoneNav('/')">🏠主页</span>
-      <span class="nav-item" @click="phoneNav('/archive')">🗃️归档</span>
-      <span class="nav-item" @click="phoneNav('/tags')">🏷️标签</span>
-      <span class="nav-item" @click="phoneNav('/links')">🔗友链</span>
-      <span class="nav-item" @click="phoneNav('/about')">❓关于</span>
+    <n-card v-if="isTablet" title="导航" content-style="display: flex; flex-wrap:wrap" class="mb-6">
+      <span class="mx-3 my-1" @click="phoneNav('/')">🏠主页</span>
+      <span class="mx-3 my-1" @click="phoneNav('/archive')">🗃️归档</span>
+      <span class="mx-3 my-1" @click="phoneNav('/tags')">🏷️标签</span>
+      <span class="mx-3 my-1" @click="phoneNav('/links')">🔗友链</span>
+      <span class="mx-3 my-1" @click="phoneNav('/about')">❓关于</span>
     </n-card>
-    <SidebarSearch />
-    <n-card v-if="hitokoto.hitokoto" title="一言">
+    <SidebarSearch class="mb-6" />
+    <n-card v-if="hitokoto.hitokoto" title="一言" class="mb-6">
       <span>{{ hitokoto.hitokoto }}</span>
-      <n-card :bordered="false" style="text-align:right">
+      <div class="text-right pt-3.5">
         <span>—— {{ hitokoto.from_who }}「{{ hitokoto.from }}」</span>
-      </n-card>
+      </div>
     </n-card>
-    <n-card title="我的歌单">
+    <n-card title="我的歌单" class="mb-6">
       <APlayer song-server="tencent" song-id="2363529455" order="random" />
     </n-card>
-    <n-card title="关于">
+    <n-card title="关于" class="mb-6">
       <div style="text-align: center;" class="about">
-        <a href="/feed.xml" target="_blank" style="display: inline-block;">
+        <a class="m-0.5 inline-block" href="/feed.xml" target="_blank">
           <i-carbon:rss style="font-size:xx-large" />
         </a>
-        <a href="https://github.com/liuly0322" target="_blank" style="display: inline-block;">
+        <a class="m-0.5 inline-block" href="https://github.com/liuly0322" target="_blank">
           <i-ant-design:github-filled style="font-size:xx-large" />
         </a>
-        <a @click="toggleDark()" style="display: inline-block;">
+        <span class="m-0.5 inline-block" @click="toggleDark()">
           <i-carbon:haze-night v-if="isDark" style="font-size:xx-large" />
           <i-carbon:sun v-if="!isDark" style="font-size:xx-large" />
-        </a>
+        </span>
         <p style="margin-top: 1em;">Copyright © 2021 2022 liuly</p>
         <p>
           License:
@@ -51,31 +65,3 @@
     </n-card>
   </div>
 </template>
-
-<script setup lang="ts">
-const { isDark, toggleDark } = useDarks()
-
-const { isTablet } = usePhone()
-const router = useRouter()
-const phoneNav = (path: string) => {
-  (document.querySelector('.nav-sider') as HTMLElement).click()
-  router.push(path)
-}
-
-const { data } = useFetch('https://v1.hitokoto.cn').json()
-const hitokoto = computed(() => ({ from: data.value?.from, hitokoto: data.value?.hitokoto, from_who: data.value?.from_who }))
-</script>
-
-<style scoped>
-.n-card {
-  margin: 0 0 24px 0;
-}
-
-.about a {
-  margin: 0 2px;
-}
-
-.nav-item {
-  margin: 4px 1em;
-}
-</style>
