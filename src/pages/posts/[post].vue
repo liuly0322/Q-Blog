a8b063f8c36c2d7c6930f87335675f4c3c4419e1
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-08-25 17:28:30 +0800

    fix: gitignore

---
+++ src/pages/posts/[post].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps<{ post: string }>()
+const url = computed(() => './' + props.post + '.json')
+const { data } = useFetch(url, {refetch: true}).json()
+</script>
+
+<template>
+  <div v-html="data" class="md-blog m-auto text-left"></div>
+</template>
