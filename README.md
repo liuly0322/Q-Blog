4d51272dd5fb861d856858009fb1ed85656eba83
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-08-25 17:06:56 +0800

    chore: 去除 python 依赖

--- README.md
+++ README.md
@@ -14,15 +14,9 @@
 
 组件库采用 naive-ui, Vue3 + TS 写起来很爽（）
 
-特性：
-
 - 响应式布局
 - 夜间模式
-
-## 问题
-
 - 目前是 SPA 应用，SEO 不友好
-- 本地构建文档暂时不完善
 
 ## 食用方法
 
@@ -49,20 +43,13 @@ clone 本仓库后，确保 node 版本号大于等于 14, 并已经安装 npm
 ```bash
 npm install -g pnpm # 如果还未安装 pnpm
 pnpm i
-pip install -r requirement.txt # 安装 python 依赖，可以在虚拟环境中进行
 ```
 
 博客写在 posts 文件夹下，md 格式
 
 需要有 YAML frontmatter, 至少包含 title, date, tags
 
-写完后： 
-
-```bash
-python parse_blogs/parse.py # 在项目根目录下运行
-```
-
-在自己浏览器上预览效果
+在自己浏览器上预览效果（目前暂时没做到热加载实时预览文章内容）
 
 ```bash
 pnpm run dev
@@ -76,4 +63,4 @@ pnpm run build
 
 即可生成静态文件在 dists 文件夹下，自行部署即可
 
-默认路由方式 History 而不是 Hash，这意味着自行部署可能需要对服务器进行相关设置，请求默认解析到 `index.html`
+默认路由方式 History 而不是 Hash，这意味着可能需要对服务器进行相关设置，将请求默认解析到 `index.html`
---
+++ parse/parse.js
@@ -0,0 +1,122 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+const frontmatter = require('frontmatter')
+const Rss = require('rss')
+const feed = new Rss({
+  title: "liuly's Blog",
+  description: 'liuly 的个人 Blog',
+  feed_url: 'https://blog.liuly.moe/feed.xml',
+  site_url: 'https://blog.liuly.moe',
+  copyright: '2021 Liuly',
+  language: 'zh-cn',
+})
+
+const prism = require('markdown-it-prism')
+const math = require('markdown-it-texmath')
+const anchor = require('markdown-it-anchor')
+const md = require('markdown-it')({
+  html: true,
+})
+  .use(prism)
+  .use(math)
+  .use(anchor)
+const md_pure = require('markdown-it')()
+
+const publicImages = path.join('public', 'images')
+const publicPosts = path.join('public', 'posts')
+
+const formatDate = (date) => {
+  return date
+    .toISOString()
+    .replace(/T/g, ' ')
+    .replace(/\.[\d]{3}Z/, '')
+}
+
+;(async () => {
+  try {
+    await fs.rm(publicImages, { recursive: true, force: true })
+    await fs.mkdir(publicImages)
+    await fs.rm(publicPosts, { recursive: true, force: true })
+    await fs.mkdir(publicPosts)
+  } catch {}
+
+  const posts = []
+
+  const files = await fs.readdir('posts')
+  for (const file of files) {
+    if (file.endsWith('.md')) {
+      const blogName = path.basename(file, '.md')
+      const blogPath = path.join('posts', file)
+      const content = await fs.readFile(blogPath, { encoding: 'utf-8' })
+      const parsed = frontmatter(content)
+      // 准备生成每一博客的 json, page.json, summary.json
+      posts.push({
+        title: parsed.data.title,
+        date: parsed.data.date,
+        tags: parsed.data.tags,
+        url: blogName,
+        content: parsed.content,
+      })
+    } else if (!path.extname(file)) {
+      // 移动 images
+      const dirname = path.join('posts', file)
+      const images = await fs.readdir(dirname)
+      for (const image of images) {
+        const src = path.join(dirname, image)
+        const dst = path.join(publicImages, image)
+        await fs.copyFile(src, dst)
+      }
+    }
+  }
+
+  posts.sort((a, b) => b.date.valueOf() - a.date.valueOf())
+  posts.forEach((post) => {
+    post.date = formatDate(post.date)
+  })
+
+  // 生成 page.json
+  /**
+   * 截断字符串，优先使用 <!-- more --> 标识
+   * @param {string} s 原字符串
+   * @param {number} len 截断长度
+   * @returns {string} 截断后的字符串
+   */
+  const truncate = (s, len) => {
+    const index = s.indexOf('<!-- more -->')
+    if (index != -1) {
+      return s.slice(0, index)
+    } else {
+      return s.length > len ? s.slice(0, len) : s
+    }
+  }
+  const pages = posts.map((post) => md.render(truncate(post.content, 100)))
+  fs.writeFile(path.join('public', 'page.json'), JSON.stringify(pages))
+
+  // 生成每篇文章的 json
+  for (const post of posts) {
+    const rendered = md.render(post.content)
+    fs.writeFile(
+      path.join(publicPosts, post.url + '.json'),
+      JSON.stringify(rendered)
+    )
+  }
+
+  // 生成 feed.xml
+  for (const post of posts) {
+    feed.item({
+      title: post.title,
+      url: 'https://blog.liuly.moe/posts/' + post.url,
+      description: md_pure.render(truncate(post.content, 100)),
+      date: post.date,
+    })
+  }
+  const xml = feed.xml()
+  fs.writeFile(path.join('public', 'feed.xml'), xml)
+
+  // 生成 summary.json
+  for (const post of posts) {
+    delete post.content
+  }
+  fs.writeFile(path.join('public', 'summary.json'), JSON.stringify(posts))
+})()
