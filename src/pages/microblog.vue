82d9eabeeff38f50a3d9599fe99b0ae634650cb0
liuly <me@liuly.moe>
2023-04-06 23:08:26 +0800

    feat: beautiful microblog!

--- src/pages/microblog.vue
+++ src/pages/microblog.vue
@@ -9,7 +9,19 @@ const { data } = useFetch('https://120.24.73.184/notes').json()
 const microBlogs = computed(() => {
   const microBlogs = (data.value ?? []).slice() as Array<MicroBlog>
   microBlogs.sort((a, b) => b.id - a.id)
-  return microBlogs
+  return microBlogs.map((microBlog) => {
+    const date = new Date(microBlog.create_time)
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const padTime = (time: number) => time.toString().padStart(2, '0')
+    return {
+      id: microBlog.id,
+      create_date: `${month} 月 ${day} 日 , ${year}`,
+      create_time: `${padTime(date.getHours())}:${padTime(date.getMinutes())}:${padTime(date.getSeconds())}`,
+      description: microBlog.description,
+    }
+  })
 })
 </script>
 
@@ -21,12 +33,29 @@ const microBlogs = computed(() => {
     size="small"
     :segmented="true"
   >
-    <n-card v-for="microBlog in microBlogs" :key="microBlog.id" title="liuly" class="text-left mb-2">
-      <template #header-extra>
+    <div v-for="microBlog in microBlogs" :key="microBlog.id" class="my-6">
+      <n-card class="w-max micro-blog-meta" content-style="padding: 0">
+        <i-carbon:calendar class="align-middle text-sm" />
+        {{ microBlog.create_date }}
+        <span class="opacity-50 font-light">| </span>
+        <i-carbon:time class="align-middle text-sm" />
         {{ microBlog.create_time }}
-      </template>
-      <!-- eslint-disable-next-line vue/no-v-html -->
-      <div v-html="microBlog.description" />
-    </n-card>
+      </n-card>
+      <n-card
+        class="text-left mb-2"
+        content-style="padding: 20px;"
+      >
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <div v-html="microBlog.description" />
+      </n-card>
+    </div>
   </n-card>
 </template>
+
+<style scoped>
+.micro-blog-meta {
+  padding: 3px 15px;
+  font-size: 16px;
+  border-bottom: 0px
+}
+</style>
