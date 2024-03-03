51ff8ce3d021dfb672cc916c7eacfbb2cfb8dbe1
liuly <me@liuly.moe>
2024-03-03 23:15:47 +0800

    style: more windicss

--- src/pages/bangumi.vue
+++ src/pages/bangumi.vue
@@ -28,17 +28,17 @@ onUnmounted(() => {
   <div v-if="animeList.length > 0" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-5">
     <div
       v-for="anime in animeList" :key="anime.subject.id"
-      class="flex flex-col justify-between rounded-lg shadow-md custom-card p-3 anime-card"
+      class="flex flex-col justify-between rounded-lg shadow-md custom-card p-3 <sm:flex-row <sm:items-center"
     >
-      <a class="mb-2 anime-card-pic" :href="`https://bgm.tv/subject/${anime.subject.id}`" target="_blank" rel="noopener noreferrer">
+      <a class="mb-2 <sm:flex-shrink-0 <sm:basis-2/5" :href="`https://bgm.tv/subject/${anime.subject.id}`" target="_blank" rel="noopener noreferrer">
         <img :src="anime.subject.images.medium" :alt="anime.subject.name" class="rounded-lg w-full">
       </a>
 
-      <div class="anime-card-content flex flex-col justify-between flex-grow">
+      <div class="<sm:ml-4 flex flex-col justify-between flex-grow">
         <div class="mt-2">
           <a
             :href="`https://bgm.tv/subject/${anime.subject.id}`" target="_blank" rel="noopener noreferrer"
-            class="text-lg font-bold"
+            class="text-lg text-hex-258fb8 font-bold"
           >
             {{ anime.subject.name_cn || anime.subject.name }}
           </a>
@@ -60,7 +60,7 @@ onUnmounted(() => {
 
         <div class="text-sm mt-4 flex flex-wrap justify-center">
           <n-rate :default-value="anime.rate / 2" readonly allow-half />
-          <span class="text-gray-500 ml-2 text-xs mt-1 anime-card-time">
+          <span class="text-gray-500 ml-2 text-xs mt-1 <sm:hidden">
             {{ timeToDate(anime.updated_at) }}
           </span>
         </div>
@@ -71,29 +71,3 @@ onUnmounted(() => {
     <n-spin size="large" />
   </div>
 </template>
-
-<style scoped>
-a {
-  color: #258fb8;
-}
-
-a:hover {
-  text-decoration: underline;
-}
-
-@media (max-width: 640px) {
-  .anime-card {
-    flex-direction: row;
-    align-items: center;
-  }
-  .anime-card-pic {
-    flex: 0 0 40%;
-  }
-  .anime-card-content {
-    margin-left: 1em;
-  }
-  .anime-card-time {
-    display: none;
-  }
-}
-</style>
