30510a00f6469f358296ae50301f0f5a37e3b5fb
liuly <me@liuly.moe>
2024-03-04 17:58:58 +0800

    fix: import

--- src/composables/usePage.ts
+++ src/composables/usePage.ts
@@ -1,6 +1,6 @@
 const { data } = useFetch('/page.json').json()
 
-const { summary } = useSummary()
+const { summary, firstPageAbstracts } = useSummary()
 const page = ref(1)
 const pageMax = computed(() => Math.ceil(summary.value.length / 10))
 
