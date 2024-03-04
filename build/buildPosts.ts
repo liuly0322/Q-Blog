ac9c69aac49a960ec974aacc90b682445bf50de7
liuly <me@liuly.moe>
2024-03-04 19:24:15 +0800

    perf: promise.all

--- build/buildPosts.ts
+++ build/buildPosts.ts
@@ -1,7 +1,7 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
 import frontmatter from 'frontmatter'
-import Rss from 'rss'
+import RSS from 'rss'
 import template from 'art-template'
 import markdownIt from 'markdown-it'
 import mdPrismPlugin from 'markdown-it-prism'
@@ -64,38 +64,39 @@ interface Post {
   content: string
 }
 
-async function collectPostsAndImages(): Promise<Post[]> {
-  const posts = []
-
-  const files = await fs.readdir('posts')
-  for (const file of files) {
-    if (file.endsWith('.md')) {
-      const blogName = path.basename(file, '.md')
-      const blogPath = path.join('posts', file)
-      const content = await fs.readFile(blogPath, { encoding: 'utf-8' })
-      const parsed = frontmatter(content)
-      posts.push({
-        title: parsed.data.title,
-        date: parsed.data.date,
-        tags: parsed.data.tags,
-        url: blogName,
-        content: parsed.content,
-      })
-    }
-    else if (!path.extname(file)) {
-      const dirname = path.join('posts', file)
-      const images = await fs.readdir(dirname)
-      for (const image of images) {
-        const src = path.join(dirname, image)
-        const dst = path.join(publicImages, image)
-        await fs.copyFile(src, dst)
-      }
+async function collectEachPostAndImages(file: string): Promise<Post | void> {
+  if (file.endsWith('.md')) {
+    const blogName = path.basename(file, '.md')
+    const blogPath = path.join('posts', file)
+    const content = await fs.readFile(blogPath, { encoding: 'utf-8' })
+    const parsed = frontmatter(content)
+    return {
+      title: parsed.data.title,
+      date: String(parsed.data.date.valueOf()),
+      tags: parsed.data.tags,
+      url: blogName,
+      content: parsed.content,
     }
   }
+  else if (!path.extname(file)) {
+    const dirname = path.join('posts', file)
+    const images = await fs.readdir(dirname)
+    for (const image of images) {
+      const src = path.join(dirname, image)
+      const dst = path.join(publicImages, image)
+      await fs.copyFile(src, dst)
+    }
+  }
+}
 
-  posts.sort((a, b) => b.date.valueOf() - a.date.valueOf())
+async function collectPostsAndImages(): Promise<Post[]> {
+  const files = await fs.readdir('posts')
+  const posts = (await Promise.all(files.map(collectEachPostAndImages))).filter(
+    (file): file is Post => file !== undefined,
+  )
+  posts.sort((a, b) => Number(b.date) - Number(a.date))
   posts.forEach((post) => {
-    post.date = formatDate(post.date)
+    post.date = formatDate(new Date(Number(post.date)))
   })
   return posts
 }
@@ -108,18 +109,14 @@ function truncate(s: string, len: number) {
     return s.length > len ? s.slice(0, len) : s
 }
 
-async function generateSitemap(posts: Post[]) {
-  const feed = new Rss({
+async function generateRSS(posts: Post[]) {
+  const feed = new RSS({
     title: 'liuly\'s Blog',
     description: 'liuly 的个人 Blog',
     site_url: SITE_URL,
     feed_url: `${SITE_URL}/feed.xml`,
     copyright: '2024 liuly',
     language: 'zh-cn',
-  })
-
-  const htmlTemplate = await fs.readFile(path.join('public', 'template.html'), {
-    encoding: 'utf-8',
   })
   for (const post of posts) {
     feed.item({
@@ -128,25 +125,35 @@ async function generateSitemap(posts: Post[]) {
       description: descriptionRenderer.render(truncate(post.content, 100)),
       date: post.date,
     })
-    const rendered = postRenderer.render(post.content)
-    await fs.writeFile(path.join(publicPosts, `${post.url}.htm`), rendered)
-    const html = template.render(htmlTemplate, {
-      title: post.title,
-      url: post.url,
-      description: post.content
-        .replace(/[#*~`><!-]/g, '')
-        .replace(/\s+/g, ' ')
-        .slice(0, 160),
-      tags: post.tags,
-      content: rendered,
-    })
-    await fs.writeFile(path.join('public', 'posts', `${post.url}.html`), html)
   }
   const xml = feed.xml()
   await fs.writeFile(path.join('public', 'feed.xml'), xml)
 }
 
-async function generateSummary(posts: Post[], firstPageAbstracts: string[]) {
+async function generateStaticPost(htmlTemplate: string, post: Post) {
+  const rendered = postRenderer.render(post.content)
+  await fs.writeFile(path.join(publicPosts, `${post.url}.htm`), rendered)
+  const html = template.render(htmlTemplate, {
+    title: post.title,
+    url: post.url,
+    description: post.content
+      .replace(/[#*~`><!-]/g, '')
+      .replace(/\s+/g, ' ')
+      .slice(0, 160),
+    tags: post.tags,
+    content: rendered,
+  })
+  await fs.writeFile(path.join('public', 'posts', `${post.url}.html`), html)
+}
+
+async function generateStaticPosts(posts: Post[]) {
+  const htmlTemplate = await fs.readFile(path.join('public', 'template.html'), {
+    encoding: 'utf-8',
+  })
+  await Promise.all(posts.map(post => generateStaticPost(htmlTemplate, post)))
+}
+
+async function generateSiteSummary(posts: Post[], firstPageAbstracts: string[]) {
   const summary = {
     posts: posts.map((post) => {
       return Object.fromEntries(Object.entries(post).filter(([k]) => k !== 'content'))
@@ -154,6 +161,10 @@ async function generateSummary(posts: Post[], firstPageAbstracts: string[]) {
     firstPageAbstracts,
   }
   await fs.writeFile(path.join('public', 'summary.json'), JSON.stringify(summary))
+}
+
+async function generatePostAbstracts(abstracts: string[]) {
+  await fs.writeFile(path.join('public', 'page.json'), JSON.stringify(abstracts))
 }
 
 async function buildPosts() {
@@ -164,8 +175,11 @@ async function buildPosts() {
 
   const posts = await collectPostsAndImages()
   const abstracts = posts.map(post => postRenderer.render(truncate(post.content, 100)))
-  await fs.writeFile(path.join('public', 'page.json'), JSON.stringify(abstracts))
 
-  await generateSitemap(posts)
-  await generateSummary(posts, abstracts.slice(0, 10))
+  await Promise.all([
+    generateRSS(posts),
+    generateStaticPosts(posts),
+    generatePostAbstracts(abstracts),
+    generateSiteSummary(posts, abstracts.slice(0, 10)),
+  ])
 }
