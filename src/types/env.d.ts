947924c71b88c2c901cb6e9b92eba906bd1ec53e
liuly <me@liuly.moe>
2024-03-04 23:42:07 +0800

    fix(chore): typescript

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client" />
 /// <reference types="vite-plugin-pwa/client" />
+/// <reference types="unplugin-icons/types/vue" />
 
 declare module 'frontmatter' {
   export default function frontmatter(content: string): {
