63d5c507174925eff751999fb339503fd396db85
liuly <me@liuly.moe>
2024-03-04 09:51:51 +0800

    style: remove unused div

--- src/pages/links.vue
+++ src/pages/links.vue
@@ -7,13 +7,11 @@ const friends = useFetch('/friends.json').json().data
     友情链接
   </n-divider>
   <div class="overflow-hidden flex flex-wrap">
-    <a v-for="friend in friends" :key="friend.name" :href="friend.url" target="_blank" class="flex-auto text-left m-2.5">
-      <div class="flex rounded-lg shadow-md custom-card p-4">
-        <img :src="friend.avatar" :alt="friend.name" class="mr-3 rounded-full" height="48" width="48">
-        <div class="flex flex-col">
-          <b>{{ friend.name }}</b>
-          {{ friend.introduction }}
-        </div>
+    <a v-for="friend in friends" :key="friend.name" :href="friend.url" target="_blank" class="flex-auto text-left m-2.5 flex rounded-lg shadow-md custom-card p-4">
+      <img :src="friend.avatar" :alt="friend.name" class="mr-3 rounded-full" height="48" width="48">
+      <div class="flex flex-col">
+        <b>{{ friend.name }}</b>
+        {{ friend.introduction }}
       </div>
     </a>
   </div>
