2d54a2175823100985b8d1b986cbea8605a8dde8
liuly <me@liuly.moe>
2024-03-05 12:06:50 +0800

    style: remove useless key attr

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -8,16 +8,16 @@ const { enableToc } = useToc()
 
 <template>
   <div>
-    <div v-if="isMobile" key="nav" class="flex flex-wrap card p-4 mb-6 mx-1.25">
+    <div v-if="isMobile" class="flex flex-wrap card p-4 mb-6 mx-1.25">
       <router-link v-for="(v, k, i) in mobileNavigation" :key="i" class="mx-3 my-1" :to="v">
         {{ k }}
       </router-link>
     </div>
     <div v-if="!isMobile">
-      <Toc v-show="enableToc" key="toc" />
-      <Hitokoto v-show="!enableToc" key="hitokoto" />
+      <Toc v-show="enableToc" />
+      <Hitokoto v-show="!enableToc" />
     </div>
-    <APlayer key="aplayer" song-server="netease" song-id="373425292" />
-    <ControlPanel key="panel" />
+    <APlayer song-server="netease" song-id="373425292" />
+    <ControlPanel />
   </div>
 </template>
