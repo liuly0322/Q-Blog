e0411500f183911a6404c704db255340c13904cb
liuly <me@liuly.moe>
2024-03-14 09:32:37 +0800

    revert: pwa auto fresh

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { registerSW } from 'virtual:pwa-register'
+
 import 'virtual:windi-base.css'
 import 'virtual:windi-components.css'
 import './styles/main.css'
@@ -8,6 +10,8 @@ import 'virtual:windi-devtools'
 import App from './App.vue'
 import router from './modules/router'
 import nprogress from './modules/nprogress'
+
+registerSW({ immediate: true })
 
 const app = createApp(App)
 app.use(router)
