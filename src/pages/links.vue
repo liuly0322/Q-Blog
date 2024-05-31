7d036e0cf73afd3d9421fded25fdfd40ce1f93ca
liuly <me@liuly.moe>
2024-05-31 10:59:27 +0800

    fix: friend links image height

--- src/pages/links.vue
+++ src/pages/links.vue
@@ -8,7 +8,7 @@ import friends from '~/jsons/friends.json'
   </n-divider>
   <div class="overflow-hidden flex flex-wrap">
     <a v-for="friend in friends" :key="friend.name" :href="friend.url" target="_blank" class="flex-auto text-left m-2.5 flex card p-4">
-      <img :src="friend.avatar" :alt="friend.name" class="mr-3 rounded-full" height="48" width="48">
+      <img :src="friend.avatar" :alt="friend.name" class="mr-3 rounded-full" style="aspect-ratio: 1;" height="48" width="48">
       <div class="flex flex-col">
         <b>{{ friend.name }}</b>
         {{ friend.introduction }}
