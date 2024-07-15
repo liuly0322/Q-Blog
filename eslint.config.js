b20f30e2cdef8414be258955ef8645d2f04ee531
liuly <me@liuly.moe>
2024-07-15 18:37:47 +0800

    chore: fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,8 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu({
+  rules: {
+    'ts/no-unused-expressions': ['off'],
+    'unused-imports/no-unused-vars': ['off'],
+  },
+})
