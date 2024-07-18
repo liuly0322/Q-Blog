cb26b2bcc167b577fa100352e23a35b895d53cde
liuly <me@liuly.moe>
2024-07-18 16:52:10 +0800

    fix sidebar and header position

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,10 @@ if (import.meta.hot)
 
 <template>
   <n-config-provider :theme="isDark ? darkTheme : null" :theme-overrides="isDark ? darkOverrides : null">
-    <Header />
+    <Header class="mb-4 h-16 box-border" />
     <div class="flex">
-      <Sidebar id="sidebar" class="min-w-[256px] w-[256px] sticky top-20 <lg:fixed <lg:top-[64.8px] <lg:right-[-300px] <lg:z-3 lg:mr-4 h-full duration-300 bg-hex-fff dark:bg-hex-18181c" />
-      <main class="flex-grow min-w-0 mt-4 pb-10 text-center text-gray-700 dark:text-gray-200">
+      <Sidebar id="sidebar" class="min-w-[256px] w-[256px] sticky top-20 <lg:fixed <lg:top-16 <lg:right-[-300px] <lg:z-3 lg:mr-4 h-full duration-300 bg-hex-fff dark:bg-hex-18181c" />
+      <main class="flex-grow min-w-0 pb-10 text-center text-gray-700 dark:text-gray-200">
         <router-view />
         <div
           class="lg:hidden mdui-overlay"
