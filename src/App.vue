75a9a7aee2e3ebb6c8e73e05761d72e7a552246f
liuly <me@liuly.moe>
2024-07-19 18:05:44 +0800

    fix sidebar music menu overflow

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ if (import.meta.hot)
   <n-config-provider :theme="isDark ? darkTheme : null" :theme-overrides="isDark ? darkOverrides : null">
     <Header class="mb-4 h-16 box-border" />
     <div class="flex">
-      <Sidebar id="sidebar" class="min-w-[256px] w-[256px] sticky top-20 <lg:fixed <lg:top-16 <lg:right-[-300px] <lg:z-3 lg:mr-4 h-full duration-300 bg-hex-fff dark:bg-hex-18181c" />
+      <Sidebar id="sidebar" class="<lg:fixed <lg:right-[-300px] w-[256px] flex-shrink-0 sticky top-20 <lg:top-16 overflow-scroll h-full lg:max-h-[80vh] <lg:z-3 lg:mr-4 duration-300 bg-hex-fff dark:bg-hex-18181c" />
       <main class="flex-grow min-w-0 pb-10 text-center text-gray-700 dark:text-gray-200">
         <router-view />
         <div
@@ -19,7 +19,7 @@ if (import.meta.hot)
           @click="() => toggleSidebar()"
         />
       </main>
-      <Toc class="min-w-[256px] w-[256px] sticky top-20 <xl:hidden ml-4 h-full overflow-scroll max-h-[80vh]" />
+      <Toc class="<xl:hidden w-[256px] flex-shrink-0 sticky top-20 overflow-scroll max-h-[80vh] ml-4" />
     </div>
   </n-config-provider>
 </template>
